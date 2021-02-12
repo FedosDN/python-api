97659bd26e54807130f731c76a253e81f16f15bb
Dmitry Zharikov <zdimon77@gmail.com>
2021-02-12 13:28:08 +0200

    commit from zdimon77@gmail.com ----

--- flaskapp/application/__init__.py
+++ flaskapp/application/__init__.py
@@ -1,3 +1,3 @@
-import application.http.responses.error_handlers
-import application.http.resources
-import application.profile.views
+from  .http.responses import error_handlers
+from  .http import resources
+from  .profile import views
